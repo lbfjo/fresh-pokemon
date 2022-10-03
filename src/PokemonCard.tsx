2f8eb8dc1497a403f406cc5fcedbf07433ebbfa1
Francisco Bruno <franciscobruno@MBPdeFrancisco.lan>
2022-10-03 22:31:57 +0100

    added baserepo

---
+++ src/PokemonCard.tsx
@@ -0,0 +1,22 @@
+import { h } from "preact";
+import { tw } from "twind";
+import { Pokemon } from "../utils/types.ts";
+
+export default function PokemonCard({ pokemon }: { pokemon: Pokemon }) {
+  return (
+    <div class={tw`rounded-xl border-1 p-5`}>
+      <div class={tw`text-2xl font-bold mb-2`}>{pokemon.name}</div>
+      <img src={pokemon.image} />
+      <div class={tw`mt-5 flex`}>
+        <a class={tw`underline flex-grow`} href={`/pokemon/${pokemon.id}`}>
+          View Details...
+        </a>
+        {/* {allowAdd && (
+          <div class={tw`flex-end`}>
+            <AddToList pokemon={pokemon} />
+          </div>
+        )} */}
+      </div>
+    </div>
+  );
+}

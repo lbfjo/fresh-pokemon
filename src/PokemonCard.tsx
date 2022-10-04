10e1ae49f25f13004855071510427ad71856438d
Francisco Bruno <franciscobruno@MacBook-Pro-de-Francisco.local>
2022-10-05 00:21:31 +0100

    added island ofr client side rendering

--- src/PokemonCard.tsx
+++ src/PokemonCard.tsx
@@ -1,8 +1,16 @@
 import { h } from "preact";
 import { tw } from "twind";
-import { Pokemon } from "../utils/types.ts";
 
-export default function PokemonCard({ pokemon }: { pokemon: Pokemon }) {
+import { Pokemon } from "../utils/types.ts";
+import AddToList from "../islands/AddToList.tsx";
+
+export default function PokemonCard({
+  pokemon,
+  allowAdd,
+}: {
+  pokemon: Pokemon;
+  allowAdd?: boolean;
+}) {
   return (
     <div class={tw`rounded-xl border-1 p-5`}>
       <div class={tw`text-2xl font-bold mb-2`}>{pokemon.name}</div>
@@ -11,11 +19,11 @@ export default function PokemonCard({ pokemon }: { pokemon: Pokemon }) {
         <a class={tw`underline flex-grow`} href={`/pokemon/${pokemon.id}`}>
           View Details...
         </a>
-        {/* {allowAdd && (
-          <div class={tw`flex-end`}>
+        {allowAdd && (
+          <div>
             <AddToList pokemon={pokemon} />
           </div>
-        )} */}
+        )}
       </div>
     </div>
   );
